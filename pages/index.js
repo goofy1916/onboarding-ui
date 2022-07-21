de67bc9cccefd0754572b3f3e52593c229ae715d
goofy1610 <piyush.dongre16@gmail.com>
2022-07-21 16:20:50 +0530

    Fixed imports

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import { useState } from 'react';
-import ProcessStepper from '../Components/ProcessStepper';
-import SuccessStep from '../Components/SuccessStep';
+import ProcessStepper from '../components/ProcessStepper';
+import SuccessStep from '../components/SuccessStep';
 import UsageType from '../components/UsageType';
-import UserNameInfo from '../Components/UserNameInfo';
-import WorkplaceInfo from '../Components/WorkplaceInfo';
+import UserNameInfo from '../components/UserNameInfo';
+import WorkplaceInfo from '../components/WorkplaceInfo';
 
 export default function Home() {
   const [currentStep, setCurrentStep] = useState(1);
@@ -16,27 +16,38 @@ export default function Home() {
   return (
     <div className='container mx-auto'>
       <div className='mx-2 md:w-2/3 md:mx-auto'>
-      <div className='mt-10 relative w-full h-16'>
-        <Image layout="fill" objectFit='contain' alt="logo" src={"/logo.PNG"}/>
-      </div>
-      <ProcessStepper step={currentStep} setCurrentStep={setCurrentStep} />
-      {currentStep == 1 && <UserNameInfo
-        userName={userName}
-        setUserName={setUserName}
-        fullName={fullName}
-        setFullName={setFullName}
-        setCurrentStep={setCurrentStep}
-        />}
-      {currentStep == 2 && <WorkplaceInfo 
-        workplaceName={workplaceName}
-        setWorkplaceName={setWorkplaceName}
-        workplaceURL={workplaceURL}
-        setWorkplaceURL={setWorkplaceURL}
-        setCurrentStep={setCurrentStep}
-        />}
-      {currentStep == 3 && <UsageType setCurrentStep={setCurrentStep}/>}
-      {currentStep == 4 && <SuccessStep userName={userName} setCurrentStep={setCurrentStep}/>}
+        <div className='mt-10 relative w-full h-16'>
+          <Image
+            layout='fill'
+            objectFit='contain'
+            alt='logo'
+            src={'/logo.PNG'}
+          />
         </div>
+        <ProcessStepper step={currentStep} setCurrentStep={setCurrentStep} />
+        {currentStep == 1 && (
+          <UserNameInfo
+            userName={userName}
+            setUserName={setUserName}
+            fullName={fullName}
+            setFullName={setFullName}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep == 2 && (
+          <WorkplaceInfo
+            workplaceName={workplaceName}
+            setWorkplaceName={setWorkplaceName}
+            workplaceURL={workplaceURL}
+            setWorkplaceURL={setWorkplaceURL}
+            setCurrentStep={setCurrentStep}
+          />
+        )}
+        {currentStep == 3 && <UsageType setCurrentStep={setCurrentStep} />}
+        {currentStep == 4 && (
+          <SuccessStep userName={userName} setCurrentStep={setCurrentStep} />
+        )}
+      </div>
     </div>
   );
 }
